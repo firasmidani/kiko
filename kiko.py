5c46a6d6ac19a2b7f20b2a8c7d9f4019ea6b064a
Firas Said Midani <firasmidani@gmail.com>
2020-06-05 17:47:12 -0500

    FUNC largeTickLabels() and PrettyGrid()

--- kiko.py
+++ kiko.py
@@ -2,4 +2,21 @@
 
 import matplotlib.pyplot as plt
 
+# TABLE OF CONTENTS
 
+# largeTickLabels
+# prettyGrid
+
+def largeTickLabels(ax,fontsize=20):
+    
+    [ii.set(fontsize=fontsize) for ii in ax.get_xticklabels()];
+    [ii.set(fontsize=fontsize) for ii in ax.get_yticklabels()];
+    
+    return ax
+
+def prettyGrid(ax,color=(0.8,0.8,0.8),lw=1.5):
+
+    for ss in ax.spines:
+        ax.spines[ss].set(color=color,lw=lw)   
+
+    ax.grid(color=color,lw=lw)
